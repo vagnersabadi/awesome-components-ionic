d061992b19e39b4f68926cf3d136446e8f5995ef
Vagner <vagnerbuzatta@gmail.com>
2019-11-07 23:18:17 -0300

    Login slider

--- src/app/pages/yannbf/components/login/login-slider/login-slider.page.ts
+++ src/app/pages/yannbf/components/login/login-slider/login-slider.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides, LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-slider',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-slider.page.scss'],
 })
 export class LoginSliderPage implements OnInit {
+  // Slider methods
+  @ViewChild('slider', { static: true }) slider: IonSlides;
+  @ViewChild('innerSlider', { static: true }) innerSlider: IonSlides;
 
-  constructor() { }
+  public loginForm: any;
+  public backgroundImage = 'assets/img/background/background-6.jpg';
 
-  ngOnInit() {
+  constructor(
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+  ) { }
+
+  ngOnInit() { }
+  
+
+  goToLogin() {
+    this.slider.slideTo(1);
+  }
+
+  goToSignup() {
+    this.slider.slideTo(2);
+  }
+
+  slideNext() {
+    this.innerSlider.slideNext();
+  }
+
+  slidePrevious() {
+    this.innerSlider.slidePrev();
+  }
+
+  async presentLoading(messages: string) {
+    const loading = await this.loadingCtrl.create({
+      duration: 500,
+      message: messages,
+    });
+    await loading.present();
+
+    await loading.onDidDismiss()
+      .then(async () => {
+        const alert = await this.alertCtrl.create({
+          header: 'Logged in!',
+          subHeader: 'Thanks for logging in.',
+          buttons: ['Dismiss']
+        });
+
+        await alert.present();
+      });
+  }
+
+  login() {
+    this.presentLoading('Thanks for signing up!');
+  }
+
+  signup() {
+    this.presentLoading('Thanks for signing up!');
+  }
+
+  resetPassword() {
+    this.presentLoading('An e-mail was sent with your new password.');
   }
 
 }

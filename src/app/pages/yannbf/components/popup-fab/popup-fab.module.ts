f847de98082ee948cb31346b49952c21a88a5f25
Vagner <vagnerbuzatta@gmail.com>
2019-11-25 21:39:47 -0300

    Popup fab

--- src/app/pages/yannbf/components/popup-fab/popup-fab.module.ts
+++ src/app/pages/yannbf/components/popup-fab/popup-fab.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { PopupFabPage } from './popup-fab.page';
+import { PopupFabModalPageModule } from './popup-fab-modal/popup-fab-modal.module';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    PopupFabModalPageModule
   ],
   declarations: [PopupFabPage]
 })

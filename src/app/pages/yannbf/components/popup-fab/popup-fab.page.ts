8dcc66fa0bec9234f9234cd8afea00dd4d34713e
Vagner <vagnerbuzatta@gmail.com>
2019-11-25 21:39:47 -0300

    Popup fab

--- src/app/pages/yannbf/components/popup-fab/popup-fab.page.ts
+++ src/app/pages/yannbf/components/popup-fab/popup-fab.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { PopupFabModalPage } from './popup-fab-modal/popup-fab-modal.page';
 
 @Component({
   selector: 'app-popup-fab',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popup-fab.page.scss'],
 })
 export class PopupFabPage implements OnInit {
+  expanded: any;
+  contracted: any;
+  showIcon = true;
+  preload = true;
 
-  constructor() { }
+  constructor(
+    public modalCtrl: ModalController,
+
+  ) { }
 
   ngOnInit() {
+
+  }
+
+  async expand() {
+    this.expanded = true;
+    this.contracted = !this.expanded;
+    this.showIcon = false;
+    // setTimeout(() => {
+    //   const modal = this.modalCtrl.create('PopupFabModalPage');
+    //   modal.onDidDismiss(data => {
+    //     this.expanded = false;
+    //     this.contracted = !this.expanded;
+    //     setTimeout(() => this.showIcon = true, 330);
+    //   });
+    //   modal.present();
+    // }, 200);
+
+    setTimeout(async () => {
+
+      const modal = await this.modalCtrl.create({
+        component: PopupFabModalPage
+      });
+      await modal.present();
+
+      const { data } = await modal.onWillDismiss();
+      this.expanded = false;
+      this.contracted = !this.expanded;
+      setTimeout(() => this.showIcon = true, 330);
+
+    }, 200);
+
+
+
   }
 
 }

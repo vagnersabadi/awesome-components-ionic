5d31a016395af255c00c9a25a52652f502f4f296
Vagner <vagnerbuzatta@gmail.com>
2019-12-05 22:57:47 -0300

    Slide-nested

--- src/app/pages/yannbf/components/slides/slide-nested/slide-nested.page.ts
+++ src/app/pages/yannbf/components/slides/slide-nested/slide-nested.page.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideNestedPage implements OnInit {
 
+  slides = [{
+    title: 'Dream\'s Adventure',
+    imageUrl: 'assets/yannbf/img/lists/wishlist-1.jpg',
+  },
+  {
+    title: 'For the Weekend',
+    imageUrl: 'assets/yannbf/img/lists/wishlist-2.jpg',
+  },
+  {
+    title: 'Family Time',
+    imageUrl: 'assets/yannbf/img/lists/wishlist-3.jpg',
+  },
+  {
+    title: 'My Trip',
+    imageUrl: 'assets/yannbf/img/lists/wishlist-4.jpg',
+  }
+  ];
+
+  slideOpts = {
+    direction: 'vertical',
+    slidesPerView: 1,
+  };
+
   constructor() { }
 
   ngOnInit() {

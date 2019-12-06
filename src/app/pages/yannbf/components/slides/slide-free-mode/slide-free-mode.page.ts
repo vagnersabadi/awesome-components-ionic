c21a808eceb9b151e536a82357e5f50ec77fc804
Vagner <vagnerbuzatta@gmail.com>
2019-12-05 22:39:58 -0300

    Slide-free-mode

--- src/app/pages/yannbf/components/slides/slide-free-mode/slide-free-mode.page.ts
+++ src/app/pages/yannbf/components/slides/slide-free-mode/slide-free-mode.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-slide-free-mode',
@@ -7,7 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideFreeModePage implements OnInit {
 
-  constructor() { }
+  slides = [
+    {
+      title: 'Dream\'s Adventure',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-1.jpg',
+      songs: 2,
+      private: false
+    },
+    {
+      title: 'For the Weekend',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-2.jpg',
+      songs: 4,
+      private: false
+    },
+    {
+      title: 'Family Time',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-3.jpg',
+      songs: 5,
+      private: true
+    },
+    {
+      title: 'My Trip',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-4.jpg',
+      songs: 12,
+      private: true
+    }
+  ];
+
+  slideOpts = {
+    effect: 'fade',
+    freeMode: true,
+    loop: true,
+    speed: 1500,
+    slidesPerView: 2,
+    spaceBetween: 10,
+  };
+
+  constructor() {
+    for (let i = 0; i < 20; i++) {
+      this.slides.push(this.slides[i % 4]);
+    }
+  }
 
   ngOnInit() {
   }

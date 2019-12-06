0ff1116f9483c01e0ef7d54c8f255e47c21d7c54
Vagner <vagnerbuzatta@gmail.com>
2019-12-05 23:01:39 -0300

    slide-photo-gallery

--- src/app/pages/yannbf/components/slides/slide-photo-gallery/slide-photo-gallery.page.ts
+++ src/app/pages/yannbf/components/slides/slide-photo-gallery/slide-photo-gallery.page.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlidePhotoGalleryPage implements OnInit {
 
+  slides = [
+    {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/1',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/2',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/3',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/4',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/5',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/6',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/7',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/8',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/9',
+    }, {
+      imageUrl: 'http://lorempixel.com/1200/1200/nature/10',
+    },
+  ];
+  
   constructor() { }
 
   ngOnInit() {

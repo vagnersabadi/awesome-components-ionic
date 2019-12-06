9fb0748f3d0e4e2536f43074746aa6460c691b1c
Vagner <vagnerbuzatta@gmail.com>
2019-12-05 23:44:37 -0300

    slide-walkthrough

--- src/app/pages/yannbf/components/slides/slide-walkthrough/slide-walkthrough.page.ts
+++ src/app/pages/yannbf/components/slides/slide-walkthrough/slide-walkthrough.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-slide-walkthrough',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideWalkthroughPage implements OnInit {
 
+  @ViewChild('slider', { static: true }) slider: IonSlides;
+  slideIndex = 0;
+  slides = [
+    {
+      title: 'Dream\'s Adventure',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-1.jpg',
+      description: 'Take a look at our amazing options',
+    },
+    {
+      title: 'For the Weekend',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-2.jpg',
+      description: 'Take a look at our amazing options',
+    },
+    {
+      title: 'Family Time',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-3.jpg',
+      description: 'Take a look at our amazing options',
+    },
+    {
+      title: 'My Trip',
+      imageUrl: 'assets/yannbf/img/lists/wishlist-4.jpg',
+      description: 'Take a look at our amazing options',
+    }
+  ];
+
+  slideOpts1 = {
+    autoplay: {
+      delay: 3000,
+    },
+  };
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSlideChanged() {
+    this.slider.getActiveIndex().then((index) => {
+      console.log('Slide changed! Current index is', index);
+      this.slideIndex  = index;
+    });
+  }
+
+  goToApp() {
+    console.log('Go to App clicked');
+  }
+
+  skip() {
+    console.log('Skip clicked');
   }
 
 }

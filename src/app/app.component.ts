e1df0c9d7759bba89ed91bf74d91357776fca5ed
Vagner <vagnerbuzatta@gmail.com>
2019-12-09 22:47:17 -0300

    News Menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import 'hammerjs';
@@ -38,9 +38,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public menuCtrl: MenuController
   ) {
     this.initializeApp();
+    // menuCtrl.enable(false, 'menu-components');
   }
 
   initializeApp() {

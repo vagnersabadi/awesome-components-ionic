5a72a25ba37274b447add10294aad03907ea2a30
vagnersabadi <vagnerbuzatta@hotmail.com>
2020-01-30 22:23:37 -0300

    feat(reactive-list): new component

--- src/app/pages/yannbf/components/lists/reactive-list-label/reactive-list-label.page.ts
+++ src/app/pages/yannbf/components/lists/reactive-list-label/reactive-list-label.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { IonContent, IonCard } from '@ionic/angular';
+import { LIST_DATA, IDataList } from './list-data';
+import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-reactive-list-label',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-list-label.page.scss'],
 })
 export class ReactiveListLabelPage implements OnInit {
+  public dataList = LIST_DATA;
+  public currentCategory: string = this.dataList[0].category;
 
-  constructor() { }
+  constructor(
+    private ngZone: NgZone
+
+  ) { }
 
   ngOnInit() {
+  }
+
+  scroll(event){
+    console.log(event);
+  
+    
+    // this.itemIsVisible(event)
+  }
+
+  public itemIsVisible($event, book?: IDataList) {
+    console.log(book, $event);
+    // book.visible = $event;
+    this.setCategory();
+  }
+
+  private setCategory() {
+    const obj = {};
+    let qtdVisibleCategory = 0;
+    let higherCategory = '';
+    this.dataList
+      .filter(bk => bk.visible)
+      .forEach(
+        bk => (obj[bk.category] = obj[bk.category] ? obj[bk.category] + 1 : 1)
+      );
+    for (const key in obj) {
+      if (obj[key] > qtdVisibleCategory) {
+        qtdVisibleCategory = obj[key];
+        higherCategory = key;
+      }
+    }
+
+    this.ngZone.runOutsideAngular(() => {
+      this.ngZone.run(() => {
+        this.currentCategory = higherCategory;
+      });
+    });
   }
 
 }

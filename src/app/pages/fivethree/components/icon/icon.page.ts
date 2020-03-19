c453a1d181fb72912939ab5ec943d9f7ad84aafd
Vagner <vagnerbuzatta@gmail.com>
2020-03-19 14:49:58 -0300

    feat(icons): new component

---
+++ src/app/pages/fivethree/components/icon/icon.page.ts
@@ -0,0 +1,33 @@
+import { UtilService } from './../../../../services/util/util.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.page.html',
+  styleUrls: ['./icon.page.scss'],
+})
+export class IconPage implements OnInit {
+
+  icon = 'notifications';
+  badge = 0;
+  off = false;
+  dot = false;
+
+  constructor(private util: UtilService) {}
+
+  ngOnInit() {}
+
+  decrement() {
+    if (this.badge > 0) {
+      this.badge--;
+    }
+  }
+  increment() {
+    this.badge++;
+  }
+
+  getNewIcon() {
+    const icon = this.util.getRandomIcon();
+    this.icon = icon.icons[1] || icon.icons[0];
+  }
+}

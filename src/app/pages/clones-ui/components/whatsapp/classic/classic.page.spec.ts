d70d02334596348af53de7812947a6824ec9522b
vagnersabadi <vagnerbuzatta@hotmail.com>
2020-11-08 12:19:01 -0300

    fix: remove comments

--- src/app/pages/clones-ui/components/whatsapp/classic/classic.page.spec.ts
+++ src/app/pages/clones-ui/components/whatsapp/classic/classic.page.spec.ts
@@ -1,13 +1,3 @@
-/**
-* Ionic Full App  (https://store.enappd.com/product/ionic-full-app-ionic-4-full-app)
-*
-* Copyright © 2019-present Enappd. All rights reserved.
-*
-* This source code is licensed as per the terms found in the
-* LICENSE.md file in the root directory of this source tree.
-*/
-
-
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
--- src/app/pages/clones-ui/components/whatsapp/classic/classic.page.ts
+++ src/app/pages/clones-ui/components/whatsapp/classic/classic.page.ts
@@ -1,14 +1,4 @@
 import { ModalController } from '@ionic/angular';
-/**
-* Ionic Full App  (https://store.enappd.com/product/ionic-full-app-ionic-4-full-app)
-*
-* Copyright © 2019-present Enappd. All rights reserved.
-*
-* This source code is licensed as per the terms found in the
-* LICENSE.md file in the root directory of this source tree.
-*/
-
-
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonContent } from '@ionic/angular';

e89e9cfb2d3d7f4bbbae25c42bcb9fb5e708e3b8
vagnersabadi <vagnersabadi@users.noreply.github.com>
2022-03-07 02:28:58 +0000

    Deploying to gh-pages from @ vagnersabadi/awesome-components-ionic@28a58fb966475b2897fac6bd37b4125a4d9b7964 🚀

---
+++ 4376.d22599c0bd0ea0bb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4376],{4376:(h,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var o=s(7897),a=s(1537),c=s(7300);let n=class{constructor(t){(0,o.r)(this,t)}render(){const t=(0,a.b)(this);return(0,o.h)(o.H,{class:(0,c.c)(this.color,{[t]:!0})},(0,o.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
